4a2a1c28f60cef24234ee3f00b5bd5ce572fc52a
JSaretin <jsaretin@gmail.com>
2023-09-17 23:49:25 +0100

    added more owner functions

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -2,13 +2,12 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "./Plan.sol";
+import {Investment} from "./Investment.sol";
 
-contract Admin is Plan {
+contract Admin is Plan, Investment {
     address public owner;
 
-    constructor() {
-        owner = msg.sender;
-    }
+    constructor() {}
 
     modifier onlyOwner() {
         require(msg.sender == owner, "!owner");
@@ -39,4 +38,54 @@ contract Admin is Plan {
     }
 
     function deletePlan(uint planID) public onlyOwner {}
+
+    function _setReferralLevel(uint levelID, uint reward) internal {
+        referralReward[levelID] = reward;
+    }
+
+    function addReferralLevel(uint reward) public onlyOwner {
+        uint newLevelID = referralLevel.length;
+        _setReferralLevel(newLevelID, reward);
+        referralLevel.push(newLevelID);
+    }
+
+    function updateReferralLevel(uint levelID, uint reward) public onlyOwner {
+        _setReferralLevel(levelID, reward);
+    }
+
+    function setPauseAll(bool status) public onlyOwner {
+        setting.pauseAll = status;
+    }
+
+    function setPauseWithdraw(bool status) public onlyOwner {
+        setting.pauseWithdraw = status;
+    }
+
+    function setPauseDeposit(bool status) public onlyOwner {
+        setting.pauseDeposit = status;
+    }
+
+    function setRewardReferralOnce(bool status) public onlyOwner {
+        setting.rewardReferralOnce = status;
+    }
+
+    function setReferralContiniousReward(bool status) public onlyOwner {
+        setting.referralContiniousReward = status;
+    }
+
+    function updateSetting(
+        bool _referralContiniousReward,
+        bool _rewardReferralOnce,
+        bool _pauseAll,
+        bool _pauseWithdraw,
+        bool _pauseDeposit
+    ) public onlyOwner {
+        setting = Setting({
+            referralContiniousReward: _referralContiniousReward,
+            rewardReferralOnce: _rewardReferralOnce,
+            pauseAll: _pauseAll,
+            pauseWithdraw: _pauseWithdraw,
+            pauseDeposit: _pauseDeposit
+        });
+    }
 }
